80836b3c228daa3338a3ac5d0e22bd91e7b3126e
feelware <stevanpersonal@gmail.com>
2024-07-07 14:13:33 -0500

    feat: add reset_time_tables

--- sentinel/pb_hooks/clearTimeTables.pb.js
+++ sentinel/pb_hooks/clearTimeTables.pb.js
@@ -40,95 +40,136 @@ $app.rootCmd.addCommand(new Command({
         createRule: "",
         updateRule: "",
         deleteRule: "",
-        schema: [
-          { 
-            name: 'date_time',
-            type: 'datetime',
-            required: true,
-            options: {}
-          }, 
-          { 
-            name: 'total_logs',
-            type: 'number',
-            required: true,
-            options: {}
-          }, 
-          { 
-            name: 'min_temperature',
-            type: 'number',
-            required: true,
-            options: {}
-          }, 
-          { 
-            name: 'max_temperature',
-            type: 'number',
-            required: true,
-            options: {}
-          }, 
-          { 
-            name: 'mean_temperature',
-            type: 'number',
-            required: true,
-            options: {}
-          }, 
-          { 
-            name: 'min_humidity',
-            type: 'number',
-            required: true,
-            options: {}
-          }, 
-          { 
-            name: 'max_humidity',
-            type: 'number',
-            required: true,
-            options: {}
-          }, 
-          { 
-            name: 'mean_humidity',
-            type: 'number',
-            required: true,
-            options: {}
-          }, 
-          { 
-            name: 'min_heat_index',
-            type: 'number',
-            required: true,
-            options: {}
-          }, 
-          { 
-            name: 'max_heat_index',
-            type: 'number',
-            required: true,
-            options: {}
-          }, 
-          { 
-            name: 'mean_heat_index',
-            type: 'number',
-            required: true,
-            options: {}
-          }, 
-          { 
-            name: 'min_vpd',
-            type: 'number',
-            required: true,
-            options: {}
-          }, 
-          { 
-            name: 'max_vpd',
-            type: 'number',
-            required: true,
-            options: {}
-          }, 
-          { 
-            name: 'mean_vpd',
-            type: 'number',
-            required: true,
-            options: {}
-          }
-        ],
+        // schema: [
+        //   { 
+        //     name: 'date_time',
+        //     type: 'datetime',
+        //     required: true,
+        //     options: {}
+        //   }, 
+        //   { 
+        //     name: 'total_logs',
+        //     type: 'number',
+        //     required: true,
+        //     options: {}
+        //   }, 
+        //   { 
+        //     name: 'min_temperature',
+        //     type: 'number',
+        //     required: true,
+        //     options: {}
+        //   }, 
+        //   { 
+        //     name: 'max_temperature',
+        //     type: 'number',
+        //     required: true,
+        //     options: {}
+        //   }, 
+        //   { 
+        //     name: 'mean_temperature',
+        //     type: 'number',
+        //     required: true,
+        //     options: {}
+        //   }, 
+        //   { 
+        //     name: 'min_humidity',
+        //     type: 'number',
+        //     required: true,
+        //     options: {}
+        //   }, 
+        //   { 
+        //     name: 'max_humidity',
+        //     type: 'number',
+        //     required: true,
+        //     options: {}
+        //   }, 
+        //   { 
+        //     name: 'mean_humidity',
+        //     type: 'number',
+        //     required: true,
+        //     options: {}
+        //   }, 
+        //   { 
+        //     name: 'min_heat_index',
+        //     type: 'number',
+        //     required: true,
+        //     options: {}
+        //   }, 
+        //   { 
+        //     name: 'max_heat_index',
+        //     type: 'number',
+        //     required: true,
+        //     options: {}
+        //   }, 
+        //   { 
+        //     name: 'mean_heat_index',
+        //     type: 'number',
+        //     required: true,
+        //     options: {}
+        //   }, 
+        //   { 
+        //     name: 'min_vpd',
+        //     type: 'number',
+        //     required: true,
+        //     options: {}
+        //   }, 
+        //   { 
+        //     name: 'max_vpd',
+        //     type: 'number',
+        //     required: true,
+        //     options: {}
+        //   }, 
+        //   { 
+        //     name: 'mean_vpd',
+        //     type: 'number',
+        //     required: true,
+        //     options: {}
+        //   }
+        // ],
         indexes: [],
         options: {},
       })
+
+      collection.schema.addField(new SchemaField({
+        name: 'date_time',
+        type: 'datetime',
+        required: true,
+        options: {}
+      }))
+
+      collection.schema.addField(new SchemaField({
+        name: 'total_logs',
+        type: 'number',
+        required: true,
+        options: {}
+      }))
+
+      [ 
+        'temperature',
+        'humidity',
+        'heat_index',
+        'vpd'
+      ].forEach(v => {
+        collection.schema.addField(new SchemaField({
+          name: `min_${v}`,
+          type: 'number',
+          required: true,
+          options: {}
+        }))
+        collection.schema.addField(new SchemaField({
+          name: `max_${v}`,
+          type: 'number',
+          required: true,
+          options: {}
+        }))
+        collection.schema.addField(new SchemaField({
+          name: `mean_${v}`,
+          type: 'number',
+          required: true,
+          options: {}
+        }))
+      })
+
       $app.dao().saveCollection(collection)
     })
   },
