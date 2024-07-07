abfcfa07f7309d61d56edf65fa7c082049130792
feelware <stevanpersonal@gmail.com>
2024-07-07 00:12:59 -0500

    feat: debug record (parse datetime)

--- sentinel/pb_hooks/utils/updateTimeRows.js
+++ sentinel/pb_hooks/utils/updateTimeRows.js
@@ -6,7 +6,7 @@ const updateTimeRows = (record) => {
   $app.logger().debug('record', 
     "temperature", temperature,
     "humidity", humidity,
-    "datetime", new DateTime(datetime)
+    "datetime", typeof (new DateTime(datetime))
   )
 
   // const year = datetime.getFullYear()
