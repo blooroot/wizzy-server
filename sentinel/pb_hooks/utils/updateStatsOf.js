a084658001d650b313ea0b29dfecc3ed4fb9514d
feelware <stevanpersonal@gmail.com>
2024-07-08 18:55:50 -0500

    feat: add dates associated to min and max values of time rows

--- sentinel/pb_hooks/utils/updateStatsOf.js
+++ sentinel/pb_hooks/utils/updateStatsOf.js
@@ -3,12 +3,14 @@ module.exports = (record, logStats) => {
   const newTotalLogs = record.get('total_logs') + 1
   record.set('total_logs', newTotalLogs)
   statsList.forEach(stat => {
-    record.set(`min_${stat}`, 
-      Math.min(record.get(`min_${stat}`), logStats[stat])
-    )
-    record.set(`max_${stat}`, 
-      Math.max(record.get(`max_${stat}`), logStats[stat])
-    )
+    if (logStats[stat] < record.get(`min_${stat}`)) {
+      record.set(`min_${stat}`, logStats[stat])
+      record.set(`min_${stat}_date`, record.get('date_time'))
+    }
+    if (logStats[stat] > record.get(`max_${stat}`)) {
+      record.set(`max_${stat}`, logStats[stat])
+      record.set(`max_${stat}_date`, record.get('date_time'))
+    }
     record.set(`mean_${stat}`,
       (record.get(`mean_${stat}`) * (newTotalLogs - 1) + logStats[stat]) / newTotalLogs
     )
