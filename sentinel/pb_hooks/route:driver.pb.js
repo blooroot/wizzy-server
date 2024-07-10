9af3a66f541dc429e2fa239711c481312436293c
feelware <stevanpersonal@gmail.com>
2024-07-10 08:32:00 -0500

    test: expect robots to hold all records of robots collection

--- sentinel/pb_hooks/route:driver.pb.js
+++ sentinel/pb_hooks/route:driver.pb.js
@@ -1,7 +1,18 @@
 /// <reference path="../pb_data/types.d.ts" />
 
 routerAdd("GET", "/driver", (c) => {
-  const robotId = c.queryParam('robot')
-  $app.logger().debug('robot', 'id', robotId)
+  let robotId = c.queryParam('robot')
+  if (!robotId) {
+    const robotsCollection = $app.dao().findCollectionByNameOrId('robots')
+    const robots = $app.dao().findRecordsByFilter('robots')
+    // const newRobot = new Record(robotsCollection, {
+    //   alias: 'Robot ' + robots.length,
+    // })
+    // $app.dao().saveRecord(newRobot)
+    // robotId = newRobot.id
+    $app.logger().debug('data', 
+      'robots', robots,
+    )
+  }
   // const { x, y, dx, dy }
 })
