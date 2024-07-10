e7f1b8d37bb8b87dffc36bce3d8853b830176a28
feelware <stevanpersonal@gmail.com>
2024-07-10 08:52:05 -0500

    test: expect sql query to return all Robot records

--- sentinel/pb_hooks/route:driver.pb.js
+++ sentinel/pb_hooks/route:driver.pb.js
@@ -4,13 +4,15 @@ routerAdd("GET", "/driver", (c) => {
   let robotId = c.queryParam('robot')
   if (!robotId) {
     const robotsCollection = $app.dao().findCollectionByNameOrId('robots')
-    const allRobots = $app.dao().db().select('id').from('robots')
+    const allRobots = arrayOf(new DynamicModel({ id: '' }))
+    
+    $app.dao().db().select('id').from('robots').all(allRobots)
     // const newRobot = new Record(robotsCollection, {
     //   alias: 'Robot ' + allRobots.length,
     // })
     // $app.dao().saveRecord(newRobot)
     // robotId = newRobot.id
-    return c.json(200, JSON.stringify(allRobots))
+    return c.json(200, allRobots)
   }
   // const { x, y, dx, dy }
 })
