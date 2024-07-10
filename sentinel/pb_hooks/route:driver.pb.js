4e23832939171c147230ac9783f0c6f020554076
feelware <stevanpersonal@gmail.com>
2024-07-10 08:33:33 -0500

    fix(driver): invalid filter expression

--- sentinel/pb_hooks/route:driver.pb.js
+++ sentinel/pb_hooks/route:driver.pb.js
@@ -4,7 +4,7 @@ routerAdd("GET", "/driver", (c) => {
   let robotId = c.queryParam('robot')
   if (!robotId) {
     const robotsCollection = $app.dao().findCollectionByNameOrId('robots')
-    const robots = $app.dao().findRecordsByFilter('robots')
+    const robots = $app.dao().findRecordsByFilter('robots', '')
     // const newRobot = new Record(robotsCollection, {
     //   alias: 'Robot ' + robots.length,
     // })
