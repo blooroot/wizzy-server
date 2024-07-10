a6841133ad4451229433d2c3eac1fec3e7fd7cad
feelware <stevanpersonal@gmail.com>
2024-07-10 08:55:34 -0500

    feat: add robot id to db if not already there

--- sentinel/pb_hooks/route:driver.pb.js
+++ sentinel/pb_hooks/route:driver.pb.js
@@ -1,18 +1,16 @@
 /// <reference path="../pb_data/types.d.ts" />
 
 routerAdd("GET", "/driver", (c) => {
-  let robotId = c.queryParam('robot')
+  const robotId = c.queryParam('robot')
   if (!robotId) {
-    const robotsCollection = $app.dao().findCollectionByNameOrId('robots')
     const allRobots = arrayOf(new DynamicModel({ id: '' }))
-    
     $app.dao().db().select('id').from('robots').all(allRobots)
-    // const newRobot = new Record(robotsCollection, {
-    //   alias: 'Robot ' + allRobots.length,
-    // })
-    // $app.dao().saveRecord(newRobot)
-    // robotId = newRobot.id
-    return c.json(200, allRobots)
+    const newRobot = new Record(
+      $app.dao().findCollectionByNameOrId('robots'), 
+      { alias: 'Robot ' + allRobots.length }
+    )
+    $app.dao().saveRecord(newRobot)
+    return c.json(200, { robotId: newRobot.id })
   }
   // const { x, y, dx, dy }
 })
